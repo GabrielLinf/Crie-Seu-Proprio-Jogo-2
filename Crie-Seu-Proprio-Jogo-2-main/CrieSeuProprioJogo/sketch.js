89855f9762866efb1c2d5759b3a4ae19319af85a
GabrielLinf <105324593+GabrielLinf@users.noreply.github.com>
2024-05-17 12:11:30 -0300

    Add files via upload

---
+++ Crie-Seu-Proprio-Jogo-2-main/CrieSeuProprioJogo/sketch.js
@@ -0,0 +1,18 @@
+
+var playerIdleImg;
+var backgroundImg;
+var player;
+var bg;
+
+function setup() {
+}
+
+function preload()
+{
+  playerIdleImg = loadImage("sprites/player-idle.png");
+  backgroundImg = loadImage("sprites/background.png");
+}
+
+function draw() {
+  drawSprites();
+}
